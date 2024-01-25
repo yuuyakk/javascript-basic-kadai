32e2cbc22537dc6cac57e85ec521daebb832d659
yuuyakk <141416428+yuuyakk@users.noreply.github.com>
2024-01-25 18:44:24 +0900

    Delete kadai_005 directory

--- kadai_005/bmi.js
+++
@@ -1,4 +0,0 @@
-let weight = 68;
-let height = 1.7;
-
-console.log(weight / (height * height));
--- kadai_005/kadai_005/bmi.js
+++
@@ -1,5 +0,0 @@
-let weight = 68;
-let height = 1.7;
-let BMI = (weight / (height * height));
-
-console.log(BMI);
